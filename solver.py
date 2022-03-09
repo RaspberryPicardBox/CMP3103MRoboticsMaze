b7455ac11e2629ded2d573049b5ac125dffe5ce6
michael <raspberrypicardbox@gmail.com>
2022-03-09 12:43:19 +0000

    Removed wall follower and chose simple error minimiser

--- solver.py
+++ solver.py
@@ -27,11 +27,21 @@ class solver:
         self.orientation = odom_msg.pose.pose.orientation.z
 
     def path(self, laser_msg):
+        t = Twist()
         ranges = laser_msg.ranges
-        if not self.wall:
-            self.find_wall(ranges)
+
+        right = ranges[0]
+        left = ranges[639]
+        forward = ranges[320]
+
+        if forward > 0.5:
+            t.linear.x = 0.1
+            err = left-right
+            t.angular.z = err/10
+            self.publisher.publish(t)
         else:
-            self.follow_wall(ranges)
+            t.angular.z = 0.5
+            self.publisher.publish(t)
 
     def image_callback(self, image_data):
         cv_image = self.bridge.imgmsg_to_cv2(image_data, "bgr8")
@@ -44,48 +54,6 @@ class solver:
         cv.imshow("Camera", cv_image)
         #cv.imshow("Result", res)
         cv.waitKey(1)
-
-    def find_wall(self, ranges):
-        self.wall = False
-        forward = np.nanmean(ranges[320])
-        t = Twist()
-
-        min_dist = min(ranges)
-        min_index = ranges.index(min_dist)
-
-        err = min_index - 320
-
-        if abs(err) > 0:
-            t.angular.z = err / 100
-            self.publisher.publish(t)
-
-        if forward > 0.5:
-            t.linear.x = 0.1
-            self.publisher.publish(t)
-        else:
-            self.wall = True
-        return
-
-    def follow_wall(self, ranges):
-        t = Twist()
-        right = np.nanmean(ranges[0])
-        left = np.nanmean(ranges[639])
-        forward = np.nanmean(ranges[320])
-
-        if left < 0.3:
-            t.angular.z = 0.2
-            self.publisher.publish(t)
-            print("left is too close")
-
-        if left > 0.7:
-            t.angular.z = -0.2
-            self.publisher.publish(t)
-            print("left is too far")
-
-        if forward > 0.5:
-            t.linear.x = 0.1
-            self.publisher.publish(t)
-            print("forward")
 
 
 if __name__ == "__main__":
